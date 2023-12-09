4ee7b379537f97044f82ad91ac5d9f7662ba8e67
DuongMinhHoang <22021218@vnu.edu.vn>
2023-12-10 03:36:16 +0700

    beta1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(App.class.getResource("ChatBox.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Chat Box");
+        stage.show();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 }
